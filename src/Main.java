98d429888f9905cf20d6e09c9c7b269e91d4694b
betoayza <aaayza@gmail.com>
2023-02-02 20:28:16 -0300

    Comments good practice added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.util.Arrays;
 
+/**
+ * This program is a mix between refactoring exercises & comments good practices
+ */
+
 public class Main {
     public static void main(String[] args) {
-        int[] arrInts = {1, 2, 3, 4, 5, 6, 7};
+        int[] arrInts = {1, 2, 3, 4, 5, 6, 7}; // the initial ints array
         System.out.println("First array: " + Arrays.toString(arrInts));
 
         // Even numbers * 3
@@ -10,14 +14,16 @@ public class Main {
         System.out.println("Even numbers * 3: " + Arrays.toString(arrInts));
 
         // Get final even numbers
-        int []evenNumbers = Arrays.stream(arrInts).filter(x -> x % 2 == 0 ).toArray(); // "Functional Programming" + "Extract Variable"
+        int[] evenNumbers = Arrays.stream(arrInts).filter(x -> x % 2 == 0).toArray(); // "Functional Programming" + "Extract Variable"
         System.out.println("Final even numbers: " + Arrays.toString(evenNumbers));
     }
 
-    // Replace an iteration
+    /**
+     * This function uses the "Extract method": a refactoring technique
+     */
     public static int[] iterateArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = multiplyEvenBy3(array[i]); // "Extract Method"
+            array[i] = multiplyEvenBy3(array[i]); // "Extract Method": this is a bad comment, it's redundant
         }
         return array;
     }
